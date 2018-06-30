4cfe5b90804c45d2d7f12f3401b40b361a5e0b92
Ingmar Drewing <ingmar@drewing.de>
2018-06-30 15:11:44 +0200

    Add methods for working with named regex groups

--- cloudScraper.go
+++ cloudScraper.go
@@ -13,6 +13,9 @@ func NewCloudScraper(data string) CloudScraper {
 
 type CloudScraper interface {
 	GetFirstCapturingGroupValue() string
+	GetFirstValueOfGroupNamed(groupName string) string
+	GetValuesOfGroupsNamed(groupName string) []string
+	GetAllNamedGroupMaps() []map[string]string
 	SetPattern(string)
 }
 
@@ -29,6 +32,46 @@ func (c *cloudScraper) SetPattern(pattern string) {
 		log.Fatalln("Pattern must not be an empty string")
 	}
 	c.pattern = regexp.MustCompile(pattern)
+}
+
+// Returns the value of a named capturing group
+// defined within the regex, like the name "year" in
+// this pattern: 'yadda yadda (?P<year>\d{4}) yadda'
+func (c *cloudScraper) GetFirstValueOfGroupNamed(groupName string) string {
+	groupMap := c.GetAllNamedGroupMaps()[0]
+	if match, ok := groupMap[groupName]; ok {
+		return match
+	}
+	return ""
+}
+
+// Returns an array of all matches found for the
+// capturing group, which matches the param groupName
+func (c *cloudScraper) GetValuesOfGroupsNamed(groupName string) []string {
+	values := []string{}
+	for _, m := range c.GetAllNamedGroupMaps() {
+		if match, ok := m[groupName]; ok {
+			values = append(values, match)
+		}
+	}
+	return values
+}
+
+// Returns an array of maps of all group names as keys
+// and their matches as values found in all matches
+func (c *cloudScraper) GetAllNamedGroupMaps() []map[string]string {
+	names := c.pattern.SubexpNames()
+	matches := c.pattern.FindAllStringSubmatch(c.data, -1)
+	maps := []map[string]string{}
+
+	for _, mgroup := range matches {
+		matchMap := map[string]string{}
+		for i, match := range mgroup {
+			matchMap[names[i]] = match
+		}
+		maps = append(maps, matchMap)
+	}
+	return maps
 }
 
 // Returns the first capturing group
--- cloudScraper_test.go
+++ cloudScraper_test.go
@@ -13,3 +13,34 @@ func TestGetFirstCapturingGroupValue(t *testing.T) {
 		t.Error("Expected", expected, "but got", actual)
 	}
 }
+
+func TestGetFirstNamedGroupValue(t *testing.T) {
+	c := NewCloudScraper("asdf assfpa9sf o<Wurst>823<Wurst>123 123 <Wurst>824<Wurst>123o")
+	c.SetPattern(`st>(?P<wurst>[^<]+)<`)
+
+	expected := "823"
+	actual := c.GetFirstValueOfGroupNamed("wurst")
+
+	if actual != expected {
+		t.Error("Expected", expected, "but got", actual)
+	}
+}
+
+func TestGetAllNamedGroupValues(t *testing.T) {
+	c := NewCloudScraper("asdf assfpa9sf o<Wurst>823</Wurst>123 123 <Wurst>824</Wurst>123o")
+	c.SetPattern(`st>(?P<wurst>[^<]+)</`)
+
+	expected := "823"
+	actual := c.GetValuesOfGroupsNamed("wurst")[0]
+
+	if actual != expected {
+		t.Error("Expected", expected, "but got", actual)
+	}
+
+	expected = "824"
+	actual = c.GetValuesOfGroupsNamed("wurst")[1]
+
+	if actual != expected {
+		t.Error("Expected", expected, "but got", actual)
+	}
+}
